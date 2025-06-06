9349dec71f71f9225ef9aac6a5865750bf195805
furkanki54 <132562462+furkanki54@users.noreply.github.com>
2025-06-06 12:06:38 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
 TELEGRAM_TOKEN = "7759276451:AAF0Xphio-TjtYyFIzahQrG3fU-qdNQuBEw"
 TELEGRAM_CHAT_ID = "-1002549376225"
 
-BINANCE_BASE_URL = "https://api.binance.com"
-SYMBOL = "BTCUSDT"  # Tek coinle başla, sonra çoğaltırız
-
-IMG_PATH = "trend_chart.png"
+BINANCE_API_BASE = "https://api.binance.com"
