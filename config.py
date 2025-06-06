419b5fc385ecac3fcd028e06e3d3d62874e1fc4b
furkanki54 <132562462+furkanki54@users.noreply.github.com>
2025-06-06 11:20:57 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+TELEGRAM_TOKEN = "7759276451:AAF0Xphio-TjtYyFIzahQrG3fU-qdNQuBEw"
+TELEGRAM_CHAT_ID = "-1002549376225"
+
+BINANCE_BASE_URL = "https://api.binance.com"
+SYMBOL = "BTCUSDT"  # Tek coinle başla, sonra çoğaltırız
+
+IMG_PATH = "trend_chart.png"
